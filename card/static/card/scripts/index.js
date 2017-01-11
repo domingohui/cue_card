f8fb94c7ed611e35aade90d14903b474c1e88b88
Domingo <domingohui@gmail.com>
2017-01-11 00:16:46 -0500

    cleaned React js code

--- card/static/card/scripts/index.js
+++ card/static/card/scripts/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+"use strict";
+
+// Maybe use babel to transcribe from ES6 later
+var React = require ('react');
+var ReactDOM = require ('react-dom');
 
 class IndexTabView extends React.Component {
-	render: function() {
-		return (
-			for courseName in this.data {
-				<li> <a href="{% url 'present_card' course %}">courseName</a></li>
-			}
-		);
+	render () {
+        const courseNames = props.data;
+        const listItems = courseName.map ( );
+		return ;//<ul>{listItems}</ul>);
 	}
 }
 
-ReactDOM.render( <IndexTabView data={data} />);
+//ReactDOM.render( <IndexTabView data={data} />, document.getElementById ('root') );
