e6973917498f28cf7776fe4d9e7b49d723169d7e
Domingo <domingohui@gmail.com>
2017-01-13 00:53:26 -0500

    Barebone reactjs FE for present_card

---
+++ card/static/card/scripts/present_card.js
@@ -0,0 +1,12 @@
+class CardDisplay extends React.Component {
+    render () {
+        return (
+            <div>HELLO WORLD!</div>
+        );
+    }
+}
+
+ReactDOM.render (
+    <CardDisplay />,
+    document.getElementById ('card_display')
+);
