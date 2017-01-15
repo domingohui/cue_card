74696753d20786068598f29bf16c2a67cc237984
Domingo <domingohui@gmail.com>
2017-01-15 00:15:47 -0500

    pass card data thru props

--- card/static/card/scripts/present_card.js
+++ card/static/card/scripts/present_card.js
@@ -1,7 +1,46 @@
+'use strict';
+
+class Card extends React.Component {
+
+    constructor () {
+        super();
+        // Store card data passed to this Card 
+        this.state = {
+            card: ( (this.props.card)? this.props.card : null ),
+            display: ( (this.state.card===null) ? '' : this.state.card.cue_side ),
+            showing_cue: true,
+        }
+    }
+
+    switch_side () {
+        if ( this.state.card.showing_cue )
+            this.setState ( { display: this.state.card.other_side } );
+        else
+            this.setState ( { display: this.state.card.cue_side } );
+        this.setState ( { showing_cue : !showing_cue } );
+    }
+
+    render () {
+         return (
+            <div onClick= {this.switch_side} >
+            { this.state.display }
+            </div>
+        );
+    }
+}
+
 class CardDisplay extends React.Component {
+    constructor () {
+        super ();
+        this.state = {
+            available: window.initialData,
+            displayed: null
+        };
+    }
+
     render () {
         return (
-            <div>{ initialData }</div>
+            <Card card={this.state.available[0]} />
         );
     }
 }
