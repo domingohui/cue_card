a62a24f2b460731b5ff1709e647930b18464df41
Domingo <domingohui@gmail.com>
2017-01-15 23:57:58 -0500

    remove uncessary state properties

--- README.md
+++ README.md
@@ -3,10 +3,20 @@
 
 `python manage.py migrate`
 
-to create DB
+to create DB.
 
+Then
+`python manage.py runserver`
+
+# React Components
+- CardDisplay: decides which card to display and updates metrics to the backend
+- Card: holds a card's data and renders it 
+
+# Django 
+- Card: a Django model to store a card's data
+- (JSON)Serializer: provides a two-way channel to fetch/update cards
 
 # ToDo
 - [ ] React templates front-end - in progress
 - [ ] Functionality to add/remove cards
-- [ ] Use cache to fetch cards
+- [ ] Django: use cache to fetch cards
--- card/static/card/scripts/present_card.js
+++ card/static/card/scripts/present_card.js
@@ -9,10 +9,11 @@ class Card extends React.Component {
         // Store card data passed to this Card 
         // Ctor is the only place to ASSIGN to this.state
         // Use this.setState({...}) elsewhere
+        this.card = props.card;
+        this.showing_cue = true;
+        // this.state only contains what is visually rendered
         this.state = {
-            card: props.card, 
             display: ( (props.card===null) ? '' : props.card.other_side ),
-            showing_cue: true,
         }
     }
 
@@ -27,16 +28,16 @@ class Card extends React.Component {
     */
 
     switch_side () {
-        if ( this.state.card.showing_cue )
-            this.setState ( { display: this.state.card.other_side } );
+        if ( this.showing_cue )
+            this.setState ( { display: this.card.other_side } );
         else
-            this.setState ( { display: this.state.card.cue_side } );
-        this.setState ( { showing_cue : !showing_cue } );
+            this.setState ( { display: this.card.cue_side } );
+        this.showing_cue = !this.showing_cue;
     }
 
     render () {
          return (
-            <div onClick= {this.switch_side} >
+            <div onClick= { () => { this.switch_side() } } >
             { this.state.display }
             </div>
         );
@@ -46,15 +47,28 @@ class Card extends React.Component {
 class CardDisplay extends React.Component {
     constructor () {
         super ();
-        this.state = {
-            available: window.initialData,
-            displayed: null
-        };
+        this.available = window.initialData;
+        this.displayed = [];
+    }
+
+    fetchCardData () {
+        // Get a random card data
+        let index = getRandomInt ( 0, this.available.length-1 );
+        let toDisplay = this.available.splice ( index, 1 );
+        // If removed 1 card successfully, put it in displayed for updating later
+        if ( toDisplay.length == 1 ) {
+            toDisplay = toDisplay[0];
+            this.displayed.push ( toDisplay );
+        }
+        return toDisplay;
+    }
+
+    updateCards () {
     }
 
     render () {
         return (
-            <Card card={this.state.available[0]} />
+            <Card card={this.fetchCardData()} />
         );
     }
 }
@@ -63,3 +77,7 @@ ReactDOM.render (
     <CardDisplay />,
     document.getElementById ('card_display')
 );
+
+function getRandomInt (min, max ) {
+    return Math.floor ( Math.random() * max + 1 );
+}
